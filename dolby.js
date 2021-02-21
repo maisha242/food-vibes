9da534d8d14d57c08ca65750d8322ee4694a9544
Adam Brewer <adamhb321@gmail.com>
2021-02-20 21:27:25 -0500

    Dolby JS fixup, looking nice ;)

--- dolby.js
+++ dolby.js
@@ -3,90 +3,101 @@ const axios = require('axios').default;
 var $ = require("jquery");
 const APIKEY = 'aLoqEU72Rz9Th9tORbixy0iQRxIVJpAG';
 //const file_path = process.env.INPUT_MEDIA_LOCAL_PATH;
-const file_path = 'C:/Users/Cumtown/Documents/BRUH/sample.wav';
-const output_path = 'C:/Users/Cumtown/Documents/BRUH/sample-enhanced.wav';
-const media_input_config = {
-  method: 'post',
-  url: 'https://api.dolby.com/media/input',
-  headers: {
-    'x-api-key': APIKEY,
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  },
-  data: {
-    url: 'dlb://in/sample.wav'
-  }
-};
+const file_path = 'C:/Users/Cumtown/Documents/BRUH/dolby/';
+const output_path = 'C:/Users/Cumtown/Documents/BRUH/dolby/';
 
 
-function download_file(filename) {
-  let media_get_config = {
-    method: 'get',
-    url: 'https://api.dolby.com/media/output',
-    headers: {
-      'x-api-key': APIKEY,
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    },
-    responseType: 'stream',
-    params: {
-      url: 'dlb://out/' + filename
-    }
-  };
-  axios(media_get_config)
-    .then(function(response) {
-      response.data.pipe(fs.createWriteStream(output_path));
-      response.data.on('error', function(error) {
-        console.log(error);
-      });
-      response.data.on('end', function() {
-        console.log('File downloaded!');
-      });
-    }).catch(function(error) {
-      console.log(error);
-    });
-}
 
-function enhance_file(filename) {
-  let media_enhance_config = {
+function enhance_file(file_name) {
+  let fpfn = file_path+file_name;
+  console.log(fpfn);
+  let media_input_config = {
     method: 'post',
-    url: 'https://api.dolby.com/media/enhance',
+    url: 'https://api.dolby.com/media/input',
     headers: {
       'x-api-key': APIKEY,
       'Content-Type': 'application/json',
       'Accept': 'application/json'
     },
     data: {
-      input: 'dlb://in/sample.wav',
-      output: 'dlb://out/${filename}'
+      url: 'dlb://in/'+file_name
     }
   };
-  axios(media_enhance_config)
-    .then(function(response) {
-      console.log(response.data.job_id);
-    }).then(download_file("sample-enhanced"))
-    .catch(function(error) {
-      console.log(error);
-    })
-}
-function upload_file() {
   axios(media_input_config).then((response)=>{
     let upload_config = {
       method: 'put',
       url: response.data.url,
-      data: fs.createReadStream(file_path),
+      data: fs.createReadStream(fpfn),
       headers: {
         'Content-Type': 'application/octet-stream',
-        'Content-Length': fs.statSync(file_path).size
+        'Content-Length': fs.statSync(fpfn).size
       }
     }
     axios(upload_config).then(function() {
-      console.log("File uploaded");
+      console.log("File uploaded!");
+      return Promise.resolve(true);
     }).then().catch(function(error) {
       console.log(error);
-    });)
-}
-//  Upload file
+    })
+  }).then(function(){
+    let split = file_name.split('.');
+    let rname = split[0];
+    let extension = split[1];
+    let media_enhance_config = {
+      method: 'post',
+      url: 'https://api.dolby.com/media/enhance',
+      headers: {
+        'x-api-key': APIKEY,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      data: {
+        input: 'dlb://in/'+file_name,
+        output: 'dlb://out/'+rname+'-enhanced.'+extension
+      }
+    };
+    axios(media_enhance_config)
+      .then(function(response) {
+        console.log(response.data.job_id);
+        console.log("File enhanced!");
+      }).then(function(){
+        let media_get_config = {
+          method: 'get',
+          url: 'https://api.dolby.com/media/output',
+          headers: {
+            'x-api-key': APIKEY,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          responseType: 'stream',
+          params: {
+            url: 'dlb://out/'+rname+'-enhanced.'+extension
+          }
+        };
+        axios(media_get_config)
+          .then(function(response) {
+            response.data.pipe(fs.createWriteStream(output_path+rname+'-enhanced.'+extension));
+            response.data.on('error', function(error) {
+              console.log(error);
+            });
+            response.data.on('end', function() {
+              console.log('File downloaded!');
+            });
+          }).catch(function(error) {
+            console.log(error);
+          });
+      }).catch(function(error) {
+        console.log(error);
+      })
+    }
+  )
 
+
+
+
+
+}
+enhance_file("sample.wav");
+//  Upload file
 
 //  Enhance file
